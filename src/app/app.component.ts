20109fc3c8cfc619d84ff6490cc4a914f6f3a1e8
olgutateo <olgutateodorescu@gmail.com>
2022-05-27 22:21:51 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first_ang';
+  newToDo = '';
+  allToDos: string[] = [];
+
+  errorMessage = '';
+  addToDo() {
+    if (!this.newToDo) {
+      this.errorMessage = "Todo can't be empty";
+      return;
+    }
+    this.errorMessage = '';
+    this.allToDos.push(this.newToDo);
+    console.log(this.allToDos);
+
+    this.newToDo = '';
+  }
+  onInput(toDo: string) {
+    this.newToDo = toDo;
+    console.log(this.newToDo);
+  }
+  removeToDo(toDo: string) {
+    this.allToDos = this.allToDos.filter((item) => item != toDo);
+  }
 }
